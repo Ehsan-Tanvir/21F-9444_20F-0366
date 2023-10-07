62444515f34e16e90f6bb4bd66686843027f3246
EhsanTanvir444 <ehsantanvir13@gmail.com>
2023-10-07 16:19:38 +0500

    Multiply function added

--- Calculator.java
+++ Calculator.java
@@ -9,7 +9,11 @@ public static Integer sub(Integer A ,Integer B)
 {
 	return A-B;
 }
-	public static void main(String[] args) {
+public static Integer sub(Integer A ,Integer B)
+{
+	return A*B;
+}	
+public static void main(String[] args) {
 		System.out.println(add(2,2));
 		System.out.println(sub(5,1));
 =======
