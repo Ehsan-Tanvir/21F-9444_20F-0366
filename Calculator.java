bcc8ae3612f62db137e0841941595f15f831bc3b
EhsanTanvir444 <ehsantanvir13@gmail.com>
2023-10-07 15:38:55 +0500

    Add the Functionality of add and sub

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,16 @@
 
 public class Calculator {
-
+public static Integer add(Integer A ,Integer B)
+{
+	return A+B;
+}
+public static Integer sub(Integer A ,Integer B)
+{
+	return A-B;
+}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(add(2,2));
+		System.out.println(sub(5,1));
 	}
 
 }
