9748bd45545228b81bf9482f610cfecac4899b0b
EhsanTanvir444 <ehsantanvir13@gmail.com>
2023-10-07 17:24:35 +0500

    Created A Java File with Add or Sub Functions

---
+++ eclipse-workspace/NewCalculator/src/NewCal.java
@@ -0,0 +1,15 @@
+public class NewCal {
+
+	public static Integer Add(Integer A, Integer B) {
+		return A + B;
+	}
+
+	public static Integer Sub(Integer A, Integer B) {
+		return A - B;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Add(2, 2));
+		System.out.println(Sub(5, 1));
+	}
+}
